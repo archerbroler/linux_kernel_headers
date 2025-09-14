c4951dbedad4deaef2845a2164e7fccc8eddcf93
broler <575705195@qq.com>
2025-09-15 06:44:44 +0800

    add v5.6-rc1

--- include/linux/swab.h
+++ include/linux/swab.h
@@ -135,9 +135,9 @@ static __inline__  __u32 __fswahb32(__u32 val)
 
 static __always_inline unsigned long __swab(const unsigned long y)
 {
-#if __BITS_PER_LONG == 64
+#if BITS_PER_LONG == 64
 	return __swab64(y);
-#else /* __BITS_PER_LONG == 32 */
+#else /* BITS_PER_LONG == 32 */
 	return __swab32(y);
 #endif
 }
