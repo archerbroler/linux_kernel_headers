30f2302c3de326fc3b74a97b48b452f50c720149
broler <575705195@qq.com>
2025-09-14 19:18:24 +0800

    add v4.17

--- include/rdma/mlx5_user_ioctl_cmds.h
+++ include/rdma/mlx5_user_ioctl_cmds.h
@@ -1,6 +1,5 @@
-/* SPDX-License-Identifier: ((GPL-2.0 WITH Linux-syscall-note) OR Linux-OpenIB) */
 /*
- * Copyright (c) 2005 Intel Corporation.  All rights reserved.
+ * Copyright (c) 2018, Mellanox Technologies inc.  All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -31,47 +30,19 @@
  * SOFTWARE.
  */
 
-#ifndef IB_USER_SA_H
-#define IB_USER_SA_H
+#ifndef MLX5_USER_IOCTL_CMDS_H
+#define MLX5_USER_IOCTL_CMDS_H
 
-#include <linux/types.h>
+#include <rdma/ib_user_ioctl_cmds.h>
 
-enum {
-	IB_PATH_GMP		= 1,
-	IB_PATH_PRIMARY		= (1<<1),
-	IB_PATH_ALTERNATE	= (1<<2),
-	IB_PATH_OUTBOUND	= (1<<3),
-	IB_PATH_INBOUND		= (1<<4),
-	IB_PATH_INBOUND_REVERSE = (1<<5),
-	IB_PATH_BIDIRECTIONAL	= IB_PATH_OUTBOUND | IB_PATH_INBOUND_REVERSE
+enum mlx5_ib_create_flow_action_attrs {
+	/* This attribute belong to the driver namespace */
+	MLX5_IB_ATTR_CREATE_FLOW_ACTION_FLAGS = (1U << UVERBS_ID_NS_SHIFT),
 };
 
-struct ib_path_rec_data {
-	__u32	flags;
-	__u32	reserved;
-	__u32	path_rec[16];
+enum mlx5_ib_alloc_dm_attrs {
+	MLX5_IB_ATTR_ALLOC_DM_RESP_START_OFFSET = (1U << UVERBS_ID_NS_SHIFT),
+	MLX5_IB_ATTR_ALLOC_DM_RESP_PAGE_INDEX,
 };
 
-struct ib_user_path_rec {
-	__u8	dgid[16];
-	__u8	sgid[16];
-	__be16	dlid;
-	__be16	slid;
-	__u32	raw_traffic;
-	__be32	flow_label;
-	__u32	reversible;
-	__u32	mtu;
-	__be16	pkey;
-	__u8	hop_limit;
-	__u8	traffic_class;
-	__u8	numb_path;
-	__u8	sl;
-	__u8	mtu_selector;
-	__u8	rate_selector;
-	__u8	rate;
-	__u8	packet_life_time_selector;
-	__u8	packet_life_time;
-	__u8	preference;
-};
-
-#endif /* IB_USER_SA_H */
+#endif
--- include/rdma/mlx5_user_ioctl_verbs.h
+++ include/rdma/mlx5_user_ioctl_verbs.h
@@ -1,6 +1,5 @@
-/* SPDX-License-Identifier: ((GPL-2.0 WITH Linux-syscall-note) OR Linux-OpenIB) */
 /*
- * Copyright (c) 2005 Intel Corporation.  All rights reserved.
+ * Copyright (c) 2018, Mellanox Technologies inc.  All rights reserved.
  *
  * This software is available to you under a choice of one of two
  * licenses.  You may choose to be licensed under the terms of the GNU
@@ -31,47 +30,14 @@
  * SOFTWARE.
  */
 
-#ifndef IB_USER_SA_H
-#define IB_USER_SA_H
+#ifndef MLX5_USER_IOCTL_VERBS_H
+#define MLX5_USER_IOCTL_VERBS_H
 
 #include <linux/types.h>
 
-enum {
-	IB_PATH_GMP		= 1,
-	IB_PATH_PRIMARY		= (1<<1),
-	IB_PATH_ALTERNATE	= (1<<2),
-	IB_PATH_OUTBOUND	= (1<<3),
-	IB_PATH_INBOUND		= (1<<4),
-	IB_PATH_INBOUND_REVERSE = (1<<5),
-	IB_PATH_BIDIRECTIONAL	= IB_PATH_OUTBOUND | IB_PATH_INBOUND_REVERSE
+enum mlx5_ib_uapi_flow_action_flags {
+	MLX5_IB_UAPI_FLOW_ACTION_FLAGS_REQUIRE_METADATA	= 1 << 0,
 };
 
-struct ib_path_rec_data {
-	__u32	flags;
-	__u32	reserved;
-	__u32	path_rec[16];
-};
+#endif
 
-struct ib_user_path_rec {
-	__u8	dgid[16];
-	__u8	sgid[16];
-	__be16	dlid;
-	__be16	slid;
-	__u32	raw_traffic;
-	__be32	flow_label;
-	__u32	reversible;
-	__u32	mtu;
-	__be16	pkey;
-	__u8	hop_limit;
-	__u8	traffic_class;
-	__u8	numb_path;
-	__u8	sl;
-	__u8	mtu_selector;
-	__u8	rate_selector;
-	__u8	rate;
-	__u8	packet_life_time_selector;
-	__u8	packet_life_time;
-	__u8	preference;
-};
-
-#endif /* IB_USER_SA_H */
