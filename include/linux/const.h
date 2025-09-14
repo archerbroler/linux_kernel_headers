62a7937b824eb58f090be371f0133b421686fb5e
broler <575705195@qq.com>
2025-09-14 18:47:36 +0800

    add v4.14.33

--- include/linux/const.h
+++ include/linux/const.h
@@ -22,15 +22,7 @@
 #define _AT(T,X)	((T)(X))
 #endif
 
-#define _UL(x)		(_AC(x, UL))
-#define _ULL(x)		(_AC(x, ULL))
-
 #define _BITUL(x)	(_AC(1,UL) << (x))
 #define _BITULL(x)	(_AC(1,ULL) << (x))
 
-#define __ALIGN_KERNEL(x, a)		__ALIGN_KERNEL_MASK(x, (__typeof__(x))(a) - 1)
-#define __ALIGN_KERNEL_MASK(x, mask)	(((x) + (mask)) & ~(mask))
-
-#define __KERNEL_DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
-
-#endif /* _LINUX_CONST_H */
+#endif /* !(_LINUX_CONST_H) */
