2569dff8bb0deed27fb9a69997d38af3ef6ce3fc
broler <575705195@qq.com>
2025-09-15 02:07:35 +0800

    add v5.10.9

--- include/linux/version.h
+++ include/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 330329
-#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
+#define LINUX_VERSION_CODE 330249
+#define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
