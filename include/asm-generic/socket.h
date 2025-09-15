2cd0b16820f42529b679d93ed2e494e90f8d5f29
broler <575705195@qq.com>
2025-09-15 09:19:00 +0800

    add v6.16

--- include/asm-generic/socket.h
+++ include/asm-generic/socket.h
@@ -145,6 +145,8 @@
 
 #define SO_RCVPRIORITY		82
 
+#define SO_PASSRIGHTS		83
+
 
 #if __BITS_PER_LONG == 64 || (defined(__x86_64__) && defined(__ILP32__))
 /* on 64-bit and x32, avoid the ?: operator */
