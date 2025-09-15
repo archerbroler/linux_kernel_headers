e1f07a2204bd09f07c018ce07febc1b3b2d285b3
broler <575705195@qq.com>
2025-09-15 10:52:03 +0800

    add v6.9

--- include/asm/setup_data.h
+++ include/asm/setup_data.h
@@ -13,13 +13,12 @@
 #define SETUP_CC_BLOB			7
 #define SETUP_IMA			8
 #define SETUP_RNG_SEED			9
-#define SETUP_KEXEC_KHO			10
-#define SETUP_ENUM_MAX			SETUP_KEXEC_KHO
+#define SETUP_ENUM_MAX			SETUP_RNG_SEED
 
 #define SETUP_INDIRECT			(1<<31)
 #define SETUP_TYPE_MAX			(SETUP_ENUM_MAX | SETUP_INDIRECT)
 
-#ifndef __ASSEMBLER__
+#ifndef __ASSEMBLY__
 
 #include <linux/types.h>
 
@@ -79,16 +78,6 @@ struct ima_setup_data {
 	__u64 size;
 } __attribute__((packed));
 
-/*
- * Locations of kexec handover metadata
- */
-struct kho_data {
-	__u64 fdt_addr;
-	__u64 fdt_size;
-	__u64 scratch_addr;
-	__u64 scratch_size;
-} __attribute__((packed));
-
-#endif /* __ASSEMBLER__ */
+#endif /* __ASSEMBLY__ */
 
 #endif /* _ASM_X86_SETUP_DATA_H */
